89a0c1b19437856871ebd3529e9fcbc2c8e774b6
cgroark <colingroark@gmail.com>
2017-12-01 13:28:20 -0800

    temp changer changed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello, front end');
+var submitButton = document.getElementById("submit");
+var input = document.getElementById("inputField");
+var output = document.getElementById("degreesOut");
+
+var convertTemp = function(){
+	output.innerHTML = (((input.value -32)/(9/5)).toFixed(2) + " degrees celsius")
+}
+
+submitButton.addEventListener("click", convertTemp);
+
+
+
+
